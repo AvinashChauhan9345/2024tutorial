776e77a836ab1f3d535f7dc609906cdbee151d74
AvinashChauhan9345 <cavinash9345@gmail.com>
2024-12-24 20:33:04 +0530

    Add files via upload

---
+++ Arithmeticoperators.py
@@ -0,0 +1,32 @@
+#arithetic operators
+print("2+3=",2+3)
+print("2-3=",2-3)
+print("2*3=",2*3)
+print("2/3=",2/3)
+print("13//3=",13//3)
+print("13%3=",13%3)
+print()
+#Relational operators
+print("2>3=",2>3)
+print("2<3=",2<3)
+print("2>=3=",2>=3)
+print("2<=3=",2<=3)
+print("2!=3=",2!=3)
+print("2==3=",2==3)
+
+#Logical operators
+#and or not
+print(2<3 and 4<5)
+print(2<3 and 4>5)
+print(2>3 and 4<5)
+print(2>3 and 4>5)
+print()
+print(2<3 or 4<5)
+print(2<3 or 4>5)
+print(2>3 or 4<5)
+print(2>3 or 4>5)
+print()
+print(not 21>3)
+print(not 1>3)
+
+
